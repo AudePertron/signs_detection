b7464cf41e749748fb87ffc77fe7b2653e73514e
FurigaJulien <73174838+FurigaJulien@users.noreply.github.com>
2021-02-13 14:50:07 +0100

    Add files via upload

---
+++ dataset_augmentation/config/config.py
@@ -0,0 +1,11 @@
+
+import os
+BASE_PATH = "C:/Users/julie/Desktop/Microsoft/multiclass_object_detection/dataset"
+POSITVE_PATH = 'C:/Users/julie/Desktop/Microsoft/multiclass_object_detection/dataset/signs'
+NEGATIVE_PATH = 'C:/Users/julie/Desktop/Microsoft/multiclass_object_detection/dataset/no_signs'
+
+MAX_PROPOSALS = 2000
+MAX_PROPOSALS_INFER = 200
+MAX_POSITIVE = 30
+MAX_NEGATIVE = 10
+INPUT_DIMS = (250, 250)
